3aadf4f43baf2d65e980a40132576788f60c0608
fufu <fufuofficial@github.com>
2026-08-12 16:51:15 +0800

    Add Kitty terminal configuration and update symlink script

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,2 +1,8 @@
+rm -rf ~/.config/nvim
 ln -s /home/fufu/dots-dms/config/nvim ~/.config/nvim
 
+rm -rf ~/.config/matugen
+ln -s /home/fufu/dots-dms/config/matugen ~/.config/matugen
+
+rm -rf ~/.config/kitty/kitty.conf
+ln -s /home/fufu/dots-dms/config/kitty/kitty.conf ~/.config/kitty/kitty.conf
