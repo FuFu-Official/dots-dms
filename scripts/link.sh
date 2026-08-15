03425d3f878f2065859bf6f4ef07c3f2aaa17ad3
fufu <fufu@email.com>
2026-08-15 21:41:16 +0800

    fix link

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,28 +1,21 @@
-rm -rf ~/.config/nvim
-ln -s /home/fufu/dots-dms/config/nvim ~/.config/nvim
+#!/usr/bin/env bash
+set -e
 
-rm -rf ~/.config/matugen
-ln -s /home/fufu/dots-dms/config/matugen ~/.config/matugen
+DOTS_PREFIX="$(cd "$(dirname "$0")/.." && pwd)"
 
-# Only link this core config file without matugen generated theme files which are dynamically changed
-rm -rf ~/.config/kitty/kitty.conf
-ln -s /home/fufu/dots-dms/config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+mkdir -p "$HOME/.config"
 
-rm -rf ~/.config/kitty/open-actions.conf
-ln -s /home/fufu/dots-dms/config/kitty/open-actions.conf ~/.config/kitty/open-actions.conf
+rm -rf "$HOME/.config/nvim"
+ln -s "$DOTS_PREFIX/config/nvim" "$HOME/.config/nvim"
 
-rm -rf ~/.config/fish
-ln -s /home/fufu/dots-dms/config/fish ~/.config/fish
+rm -rf "$HOME/.config/fish"
+ln -s "$DOTS_PREFIX/config/fish" "$HOME/.config/fish"
 
-fcitx5-remote -e 2>/dev/null || true
-sleep 1
-rm -rf ~/.config/fcitx5
-cp -a /home/fufu/dots-dms/config/fcitx5 ~/.config/fcitx5
+rm -rf "$HOME/.config/matugen"
+ln -s "$DOTS_PREFIX/config/matugen" "$HOME/.config/matugen"
 
-rm -rf ~/.local/share/fcitx5/rime/default.custom.yaml
-ln -s /home/fufu/dots-dms/config/fcitx5/rime/default.custom.yaml ~/.local/share/fcitx5/rime/default.custom.yaml
-rime_deployer --build ~/.local/share/fcitx5/rime /usr/share/rime-data ~/.local/share/fcitx5/rime/build
-fcitx5 -d
+rm -rf "$HOME/.config/kitty"
+ln -s "$DOTS_PREFIX/config/kitty" "$HOME/.config/kitty"
 
-rm -rf ~/.config/starship.toml
-ln -s /home/fufu/dots-dms/config/starship.toml ~/.config/starship.toml
+rm -rf "$HOME/.config/starship.toml"
+ln -s "$DOTS_PREFIX/config/starship.toml" "$HOME/.config/starship.toml"
